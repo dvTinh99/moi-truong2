b1b99b999da6c386b245c3bcadae935d7fb60abb
tinhdoan <tinh.doan@codecomplete.jp>
2022-06-21 16:46:09 +0700

    update menu mobile

--- wp-content/themes/tinhdoan/header.php
+++ wp-content/themes/tinhdoan/header.php
@@ -73,74 +73,15 @@
     <!-- end header-bot -->
     <nav id="menu-mobile">
         <ul>
-            <ul id="menu-menu-top-1">
-                <li style="background-color:white ;">
-                    <a href="https://baoholaodongdanang.com"><img style="width: 300px;" src="<?php bloginfo('template_directory') ?>/html/images/logo-moitruong.jpg" alt="logo_page" /></a>
-                </li>
-                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-home menu-item-11675 active">
-                    <a href="https://baoholaodongdanang.com/">Trang chủ</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11346"><a href="https://baoholaodongdanang.com/gioi-thieu/">Giới Thiệu</a></li>
-                <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-11616">
-                    <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/">Bảo hộ lao động</a>
-                    <ul class="sub-menu">
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11609">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-dau/">Thiết
-                                bị bảo vệ đầu</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11619">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-tai/">Thiết
-                                bị bảo vệ tai</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11606">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-ho-hap/">Thiết
-                                bị bảo vệ hô hấp</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11612">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/gang-tay-bao-ho-lao-dong/">Găng
-                                Tay Bảo Hộ Lao Động</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11610">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                bị bảo vệ mặt và mắt</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11608">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/trang-phuc-bao-ho-lao-dong/">Trang
-                                Phục Bảo Hộ Lao Động</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11607">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-chan/">Thiết
-                                bị bảo vệ chân</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11611">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-canh-bao-atgt/">Thiết
-                                Bị Cảnh Báo ATGT</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11613">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/an-toan-hoa-chat/">An
-                                toàn hóa chất</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11617">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-an-toan-tren-cao/thiet-bi-an-toan-tren-cao-khac/">Thiết
-                                bị an toàn trên cao khác</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11620">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/an-toan-dien/">Thiết
-                                bị
-                                an toàn điện</a>
-                        </li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11618">
-                            <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bhld-ho-tro-sx-atld-khac/thiet-bi-ho-tro-san-xuat/">Thiết
-                                bị hỗ trợ sản xuất</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11604"><a href="https://baoholaodongdanang.com/danh-muc/phong-chay-chua-chay/">Tư vấn môi
-                        trường</a>
-                </li>
-                <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-11348"><a href="https://baoholaodongdanang.com/tin-tuc-an-toan/">Tư vấn xử lí nước thải</a></li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11345"><a href="https://baoholaodongdanang.com/lien-he/">Liên Hệ</a></li>
-            </ul>
+        <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header-menu',
+                        'menu_id' => 'menu-menu-top-1',
+                        // 'menu_class' => 'menu hidden-sm hidden-xs'
+                    )
+                );
+                ?>
         </ul>
         <a class="_close _toggle-menu" href="javascript:void(0)"></a>
     </nav>
