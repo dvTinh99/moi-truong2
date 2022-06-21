501f7d991bbffe1bd3cb9cdcc20fd4a00783def8
tinhdoan <tinh.doan@codecomplete.jp>
2022-06-21 16:51:27 +0700

    update menu footer

--- wp-content/themes/tinhdoan/footer.php
+++ wp-content/themes/tinhdoan/footer.php
@@ -18,122 +18,15 @@
                                 <div class="support-ft">
                                     <h6>Sản phẩm & Dịch vụ</h6>
                                     <ul class="lst_">
-                                        <ul id="menu-san-pham-dich-vu" class="">
-                                            <li id="menu-item-11957"
-                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11957">
-                                                <a href="https://baoholaodongdanang.com/trang-chu/">Trang chủ</a>
-                                            </li>
-                                            <li id="menu-item-11967"
-                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11967">
-                                                <a href="https://baoholaodongdanang.com/gioi-thieu/">Giới Thiệu</a>
-                                            </li>
-                                            <li id="menu-item-9688"
-                                                class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-9688">
-                                                <a href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/">Bảo
-                                                    hộ lao động</a>
-                                                <ul class="sub-menu">
-                                                    <li id="menu-item-11959"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11959">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/trang-phuc-bao-ho-lao-dong/">An
-                                                            toàn hóa chất</a>
-                                                    </li>
-                                                    <li id="menu-item-11960"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11960">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-ho-hap/">Bảo
-                                                            hộ phòng sạch</a>
-                                                    </li>
-                                                    <li id="menu-item-11961"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11961">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-chan/">Găng
-                                                            tay bảo hộ lao động</a>
-                                                    </li>
-                                                    <li id="menu-item-11962"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11962">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/gang-tay-bao-ho-lao-dong/">Thiết
-                                                            bị an toàn điện</a>
-                                                    </li>
-                                                    <li id="menu-item-11963"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11963">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-canh-bao-atgt/">Thiết
-                                                            bị an toàn trên cao</a>
-                                                    </li>
-                                                    <li id="menu-item-11964"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11964">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-phong-chay-chua-chay/">Thiết
-                                                            bị bảo vệ chân</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị bảo vệ đầu</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị bảo vệ hô hấp</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị bảo vệ mặt và mắt</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị bảo vệ tai</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">THIẾT
-                                                            BỊ BHLĐ HỖ TRỢ SX, ATLĐ KHÁC</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị cảnh báo ATGT</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Thiết
-                                                            bị phòng cháy chữa cháy</a>
-                                                    </li>
-                                                    <li id="menu-item-11965"
-                                                        class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-11965">
-                                                        <a
-                                                            href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-bao-ve-mat-va-mat/">Trang
-                                                            phục bảo hộ lao động</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li id="menu-item-9852"
-                                                class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-9852">
-                                                <a
-                                                    href="https://baoholaodongdanang.com/danh-muc/bao-ho-lao-dong/thiet-bi-phong-chay-chua-chay/">Tư
-                                                    vấn môi trường</a>
-                                            </li>
-                                            <li id="menu-item-11966"
-                                                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-11966">
-                                                <a href="https://baoholaodongdanang.com/tin-tuc-an-toan-lao-dong/">Tư
-                                                    vấn xử lí nước thải</a>
-                                            </li>
-                                            <li id="menu-item-11956"
-                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11956">
-                                                <a href="https://baoholaodongdanang.com/lien-he/">Liên Hệ</a>
-                                            </li>
-                                        </ul>
+                                    <?php
+                                        wp_nav_menu(
+                                            array(
+                                                'theme_location' => 'header-menu',
+                                                'menu_id' => 'menu-san-pham-dich-vu',
+                                                // 'menu_class' => 'menu hidden-sm hidden-xs'
+                                            )
+                                        );
+                                        ?>
                                     </ul>
                                 </div>
                             </div>
